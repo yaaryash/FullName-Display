943320e008bda7e5c8f53d6d3b29aec828756358
yaaryash <yashwalke3434@gmail.com>
2024-02-23 22:41:53 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">React</header>
-    </div>
+    <form className="App">
+      <h1>Full Name Display</h1>
+      <div>
+        <label for="fullName">First Name: </label>
+        <input type="text" id="fullName" name="fav_language" value="" />
+      </div>
+      <div>
+        <label for="lastName">Last Name: </label>
+        <input type="text" id="lastName" name="fav_language" value="" />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
