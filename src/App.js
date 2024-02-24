cf3a16e6be1052bd59e13c1ecacd0e50b289174a
yaaryash <yashwalke3434@gmail.com>
2024-02-24 20:01:59 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
+import React, { useState } from "react";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
   const [lastName, setLastName] = useState("");
   const [firstName, setFirstName] = useState("");
   const [fullName, setFullName] = useState("");
-  function handleFirstName(e) {
+
+  function handleFirstNameChange(e) {
     setFirstName(e.target.value);
   }
-  function handleLastName(e) {
+
+  function handleLastNameChange(e) {
     setLastName(e.target.value);
   }
+
   function handleSubmit(e) {
     e.preventDefault();
     const fullName = `${firstName} ${lastName}`;
     setFullName(fullName);
   }
+
   return (
-    <form className="App" onChange={handleSubmit}>
+    <form className="App" onSubmit={handleSubmit}>
       <h1>Full Name Display</h1>
       <div>
-        <label for="firstName">First Name: </label>
+        <label htmlFor="firstName">First Name: </label>
         <input
           required
           type="text"
           id="firstName"
           name="firstName"
           value={firstName}
-          onChange={handleFirstName}
+          onChange={handleFirstNameChange}
         />
       </div>
       <div>
-        <label for="lastName">Last Name: </label>
+        <label htmlFor="lastName">Last Name: </label>
         <input
           required
           type="text"
           id="lastName"
           name="lastName"
           value={lastName}
-          onChange={handleLastName}
+          onChange={handleLastNameChange}
         />
       </div>
       <button type="submit">Submit</button>
